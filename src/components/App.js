fb83fa057899484b121028db9e37de067f946506
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-08-08 08:28:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import ProductDetail from './ProductDetail';
+import Admin from './Admin';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/admin">Admin</Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products/:id" component={ProductDetail} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
